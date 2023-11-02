3ba769ab6da0831f6ff2eab6c4d1bd19ad37e9e8
RomaSarkisyan <sarkisyanrom98@gmail.com>
2023-11-02 21:18:18 +0400

    added one file

---
+++ index.js
@@ -0,0 +1,17 @@
+var tiv = prompt("miat aranc amboxj tiv gri axper:");
+
+if ( tiv%2==0){
+  tiv = promt 
+
+console.log(tiv);
+}
+const tiv2 = prompt("miatel ankap tiv gri axper:"); 
+
+console.log(tiv2);
+
+
+if (tiv % tiv2 == 0) {
+  console.log("lava");
+} else {
+  console.log("lav chi");
+}
